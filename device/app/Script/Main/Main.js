4dfe3daf47778ca4e60bbca7ea1a208b3de7220d
pochaevets <pochaevets.a@gmail.com>
2015-11-11 08:57:49 +0300

    SUPS-56 Main menu and Dashboard

--- device/app/Script/Main/Main.js
+++ device/app/Script/Main/Main.js
@@ -29,14 +29,10 @@ function OpenMenu() {
 
 //----------------Begin Info Block Visit---------------------------------
 function GetToDayUnDoneRequestsCount(){//(searchText - строка поиска, getCount - получать ли количество[1-ДА,0-НЕТ])
-	var q = new Query("SELECT Document_Visit.Id " +
-			"FROM Document_Visit " +
-			"WHERE (Document_Visit.PlanStartDataTime " +
-			"BETWEEN @DateStart AND @DateEnd) " +
-			"AND Document_Visit.Status = @StatusEx");
+	var q = new Query("SELECT DE.id FROM Document_Event DE WHERE DE.ActualEndDate >= @DateStart AND DE.ActualEndDate <= @DateEnd AND DE.Status = @StatusEx");
 	
 	//q.AddParameter("StatusComp", DB.Current.Constant.VisitStatus.Completed);
-	q.AddParameter("StatusEx", DB.Current.Constant.VisitStatus.Expected);
+	q.AddParameter("StatusEx", DB.Current.Constant.StatusyEvents.Done);
 	q.AddParameter("DateStart", DateTime.Now.Date);
 	q.AddParameter("DateEnd", DateTime.Now.Date.AddDays(1));
 	return q.ExecuteCount();
@@ -44,12 +40,8 @@ function GetToDayUnDoneRequestsCount(){//(searchText - строка поиска
 }
 
 function GetToDayDoneRequestCount(){//(searchText - строка поиска, getCount - получать ли количество[1-ДА,0-НЕТ])
-	var q = new Query("SELECT Document_Visit.Id " +
-			"FROM Document_Visit " +
-			"WHERE Document_Visit.FactEndDataTime >= @DateStart " +
-			"AND Document_Visit.FactEndDataTime < @DateEnd " +
-			"AND Document_Visit.Status == @StatusComp");	
-	q.AddParameter("StatusComp", DB.Current.Constant.VisitStatus.Completed);
+	var q = new Query("SELECT DE.id FROM Document_Event DE WHERE DE.StartDatePlan >= @DateStart AND DE.StartDatePlan < @DateEnd AND DE.Status == @StatusComp");	
+	q.AddParameter("StatusComp",  DB.Current.Constant.StatusyEvents.Appointed);
 	q.AddParameter("DateStart", DateTime.Now.Date);
 	q.AddParameter("DateEnd", DateTime.Now.Date.AddDays(1));
 	return q.ExecuteCount();
