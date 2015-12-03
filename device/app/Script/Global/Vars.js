49d9239f0385f652e0d42b561f8df256b90462a0
Denis <denis@zemskix.com>
2015-12-03 18:17:44 +0300

    SUPS-57

--- device/app/Script/Global/Vars.js
+++ device/app/Script/Global/Vars.js
@@ -48,17 +48,3 @@ function getClient(){
 }
 // End Current Event ID
 
-
-
-// Begin Current Event ID
-var currentTask;
-
-function setTask(a){
-	currentTask = a;
-}
-
-function getTask(){
-	return currentTask;
-}
-// End Current Event ID
-
--- device/app/Script/Sync/Main.js
+++ device/app/Script/Sync/Main.js
@@ -4,6 +4,29 @@
 	DrawFtpReport();
 }
 
+// ------------------------ Main screen module ------------------------
+
+function CloseMenu() {
+    var sl = Variables["swipe_layout"];
+    if (sl.Index == 1) {
+        sl.Index = 0;
+    }
+    else if (sl.Index == 0) {
+        sl.Index = 1;
+    }
+}
+
+function OpenMenu() {
+    var sl = Variables["swipe_layout"];
+    if (sl.Index == 1) {
+        sl.Index = 0;
+    }
+    else if (sl.Index == 0) {
+        sl.Index = 1;
+    }
+}
+
+
 // -------------------- Sync Data ------------
 function SyncData() {
 	 $.dataSyncReport.Visible = false;
