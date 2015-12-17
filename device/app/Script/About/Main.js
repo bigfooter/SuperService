161ef8606ac57d6b366a10ce29e62f80c954c297
pochaevets <pochaevets.a@gmail.com>
2015-12-17 16:52:07 +0300

    SUPS-55 About

--- device/app/Script/About/Main.js
+++ device/app/Script/About/Main.js
@@ -8,10 +8,35 @@ function SendLog(){
 
 	// $.sendButton.Visible = true;
 	// $.dataSyncLayout.Visible = false;
-	// $.dataSyncIndicator.Stop();	
+	// $.dataSyncIndicator.Stop();
 
 	if (result)
 		Dialog.Message(Translate["#success#"]);
 	else
 		Dialog.Message(Translate["#noSuccess#"]);
+}
+
+function CloseMenu() {
+    var sl = Variables["swipe_layout"];
+    if (sl.Index == 1) {
+        sl.Index = 0;
+    }
+    else if (sl.Index == 0) {
+        sl.Index = 1;
+    }
+}
+
+function OpenMenu() {
+    var sl = Variables["swipe_layout"];
+    if (sl.Index == 1) {
+        sl.Index = 0;
+    }
+    else if (sl.Index == 0) {
+        sl.Index = 1;
+    }
+}
+
+function getAppVersion(){
+	var solVersion = new Query("SELECT Value FROM ___SolutionInfo WHERE key='version'");
+	return solVersion.ExecuteScalar();
 }
